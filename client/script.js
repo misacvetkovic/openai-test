fd6d8b2d1dec520d352ca22eda2a102f6805064b
misacvetkovic <misa.cvetkovic02@gmail.com>
2023-01-14 23:08:49 +0100

    change to live server

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5001', {
+    const response = await fetch('https://akeodex.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
